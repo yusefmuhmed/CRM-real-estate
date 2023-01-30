260e424924a73925f63e78a51747aa0c2a99d939
mohkh833 <mohkh833@gmail.com>
2023-01-30 22:12:27 +0200

    edit project route

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const Project = require('../app/controller/project.controller')
 const { auth } = require("../app/middleware/auth.middleware")
 
-router.post("/addProject", Project.insertProject)
+router.post("/addProject", Project.addProject)
 
 
 module.exports = router
